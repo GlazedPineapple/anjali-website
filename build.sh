0cddc0afab97dbf976c7a4465a87d5d9b1bcae78
GlazedPineapple <40650629+GlazedPineapple@users.noreply.github.com>
2019-10-05 23:33:03 -0400

    osx

--- build.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-echo Downloading Zola
-wget -c https://github.com/getzola/zola/releases/download/v0.9.0/zola-v0.9.0-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
-
-echo Building project
-./zola build
