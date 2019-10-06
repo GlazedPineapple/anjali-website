3ee97e44f54cdc0acf0e5a269ece5ffac67f5a01
GlazedPineapple <40650629+GlazedPineapple@users.noreply.github.com>
2019-10-05 23:16:53 -0400

    Fix curl again

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -e
+
+echo Downloading Zola
+curl -o zola.tar.gz https://github.com/getzola/zola/releases/download/v0.9.0/zola-v0.9.0-x86_64-unknown-linux-gnu.tar.gz
+
+echo Extracting Zola
+tar -xvzf zola.tar.gz
+
+echo Building project
+./zola/zola build
