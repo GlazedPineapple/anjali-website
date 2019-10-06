be0f2d524ecfabcd68344f4beca3611f70bd351d
GlazedPineapple <40650629+GlazedPineapple@users.noreply.github.com>
2019-10-05 23:22:46 -0400

    More fix

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 set -e
 
 echo Downloading Zola
-curl -o zola.tar.gz https://github.com/getzola/zola/releases/download/v0.9.0/zola-v0.9.0-x86_64-unknown-linux-gnu.tar.gz
-
-echo Extracting Zola
-tar -xvf zola.tar.gz
+wget -c https://github.com/getzola/zola/releases/download/v0.9.0/zola-v0.9.0-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
 
 echo Building project
-./zola/zola build
+./zola build
