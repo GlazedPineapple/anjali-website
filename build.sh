018858dd0c453a4df28f8e8aa82370795a781c3d
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-10-07 14:48:50 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 set -e
 
-pwd
-ls
- 
 curl -sL "https://github.com/getzola/zola/releases/download/v0.9.0/zola-v0.9.0-x86_64-unknown-linux-gnu.tar.gz" | tar zxv
-
-pwd
-ls
-
 ./zola build
 echo anjali.uses-light-the.me > public/CNAME
