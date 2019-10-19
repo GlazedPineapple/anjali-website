b536b6539222f36bf2fddf47fe7afa1ff9ba4cf2
GlazedPineapple <40650629+GlazedPineapple@users.noreply.github.com>
2019-10-18 21:28:40 -0400

    swiched to gh actions

--- build.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-curl -sL "https://github.com/getzola/zola/releases/download/v0.9.0/zola-v0.9.0-x86_64-unknown-linux-gnu.tar.gz" | tar zxv
-./zola build
-echo anjali.uses-light-the.me > public/CNAME
